b683614c92f36b744f04293f5025a1277ed5631e
Aaron Ross <ashmortar@gmail.com>
2024-08-26 23:19:13 -0700

    configure noice

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -751,11 +751,24 @@ require('lazy').setup({
 
   {
     'ellisonleao/gruvbox.nvim',
-    priority = 1000,
+    -- priority = 1000,
     config = true,
     init = function()
       vim.o.background = 'dark'
-      vim.cmd.colorscheme 'gruvbox'
+      -- vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  {
+    'gmr458/vscode_modern_theme.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('vscode_modern').setup {
+        cursorline = true,
+        transparent_background = false,
+        nvim_tree_darker = true,
+      }
+      vim.cmd.colorscheme 'vscode_modern'
     end,
   },
   -- Highlight todo, notes, etc in comments
