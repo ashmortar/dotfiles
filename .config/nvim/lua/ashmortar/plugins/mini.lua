13ef5d751fe5838bfb604dfcc369ba42c04738f0
Aaron Ross <aaron.ross@greatexpectations.io>
2024-08-28 17:39:19 -0700

    add keymaps for sessions

--- .config/nvim/lua/ashmortar/plugins/mini.lua
+++ .config/nvim/lua/ashmortar/plugins/mini.lua
@@ -1,5 +1,7 @@
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
+  version = '*',
+  lazy = false,
   config = function()
     -- Better Around/Inside textobjects
     --
@@ -32,9 +34,69 @@ return { -- Collection of various small independent plugins/modules
     -- end
 
     -- setup sessions
-    require('mini.sessions').setup()
+    local sessions = require 'mini.sessions'
+    sessions.setup()
+
+    vim.keymap.set('n', '<leader>wn', function()
+      vim.ui.input({ prompt = 'Enter  new session name: ' }, function(name)
+        if name then
+          sessions.write(name)
+        end
+      end)
+    end, { desc = 'Create a new Session' })
+
+    vim.keymap.set('n', '<leader>wl', function()
+      sessions.select 'read'
+    end, { desc = 'Load a Session' })
+
+    vim.keymap.set('n', '<leader>ws', function()
+      sessions.write()
+    end, { desc = 'Save a Session' })
+
     -- setup starter
-    require('mini.starter').setup()
+    local status, starter = pcall(require, 'mini.starter')
+    if not status then
+      return
+    end
+    starter.setup {
+      evaluate_single = true,
+      items = {
+        starter.sections.recent_files(5, false),
+        starter.sections.recent_files(5, true),
+        starter.sections.sessions(5, true),
+        starter.sections.telescope(),
+      },
+      content_hooks = {
+        starter.gen_hook.adding_bullet(),
+        starter.gen_hook.padding(3, 2),
+      },
+      footer = os.date(),
+      header = table.concat({
+        [[                                                   ]],
+        [[                                              ___  ]],
+        [[                                           ,o88888 ]],
+        [[                                        ,o8888888' ]],
+        [[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
+        [[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
+        [[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
+        [[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
+        [[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
+        [[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
+        [[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
+        [[             . ..:.::o:ooooOoCoCCC"o:o             ]],
+        [[             . ..:.::o:o:,cooooCo"oo:o:            ]],
+        [[          `   . . ..:.:cocoooo"'o:o:::'            ]],
+        [[          .`   . ..::ccccoc"'o:o:o:::'             ]],
+        [[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
+        [[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
+        [[     ...:.'.:.::::"'    . . . . .'                 ]],
+        [[    .. . ....:."' `   .  . . ''                    ]],
+        [[  . . . ...."'                                     ]],
+        [[  .. . ."'                                         ]],
+        [[ .                                                 ]],
+        [[                                                   ]],
+      }, '\n'),
+    }
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
